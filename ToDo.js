24bb581ddaf0707e5943132e84c580d82b9075b6
joaovictor <joaovictor0101@gmail.com>
2024-05-24 15:47:56 -0300

    validação feita, só falta ajeiar algumas coisas de css com relação a posicionamento

--- ToDo.js
+++ ToDo.js
@@ -1,102 +1,44 @@
-// create card
-
 /*
-function createCard(){
-    const gridCard = document.getElementById ("GridCard");
-    const Card = document.getElementById("cardContainer");
-    const clonedElement = Card.cloneNode(true);
-    gridCard.appendChild(clonedElement);
-}
+Amrmazenar os cards no local storage
 
 
 
-//const clonedElement = novoElemento.cloneNode(true);
-*/
+ clonar/ caso necessariover vidoes sobre relções entre elementos traves do dom
 
-function createCard(){
-    const gridCard = document.getElementById ("GridCard");
-    let Card = document.createElement ("div");
-    Card.setAttribute("class","Sty")
-Card.innerHTML =  ` <div id="cardContainer"class="cardStyle">
+ const gridCard = document.getElementById("GridCard");
+ */
 
-<div id="deleteCardContainer">
-    <button id="x" class="removeCardstyle"alt="remnovecard">
-        <img src="imgs to do list/remove-svgrepo-com.svg" alt="remove_card">
-        <!-- <i class="bi bi-x" style="font-size: 40px;"></i> -->
-    </button>
-</div>
+// create card
+const gridCard = document.getElementById ("GridCard");
+const ia = document.getElementById("cardContainer");
+const i = document.getElementById("nameList");
+const ButtonCard = document.getElementById("plusHover");
+const oia = document.getElementById("oia") 
+let change = 0;
+let click = 1;
 
-<!--Name list-->
 
-<div class="Area">
-    <textarea rows="2"  maxlength="25" type="text" minlength="1" placeholder="Crie sua lista"
-        class="NameListStyle"></textarea>
-</div>
+function createCard(){ 
 
-<!--Description-->
-
-<div class="Area">
-    <textarea maxlength="80" class="descriptionStyle" placeholder="description..."></textarea>
-</div>
-<!--Bar Creat Task-->
-<div class="Area">
-    <input type="text"class="BartoTaskStyle">
-</div>
-
-<!---create button task-->
-
-<div class="ButtontoTaskContainer">
-    <button class="ButtonTask">
-        <img src="imgs to do list/plus-square-svgrepo-com.svg" alt="">
-    </button>
-</div>
-<!--Gridtask-->
-<div class="AreaTaskContainer">
-
-    <!--trash button-->
-    <!-- ajetar os botões de trash na tasks -->
+    console.log(click)
+  if (click > 0 && i.value === "" ){
+    alert("add algo")
+  }else{
+      let clone = oia.cloneNode(true) 
+      gridCard.appendChild(clone)
+    i.value=""
+  }
+  
     
-        <div class="trashButtonContainer">
-            <button class="trash">
-            <img src="imgs to do list/trash-01-svgrepo-com.svg" alt="">
-            </button>
-        </div>
-        <!--no check
-        mudar ajeitar essas chack boxes 
-        -->
-        <div class="Area">
-            <button class="buttonCheckStylesAndNo">
-            <img id="" src="imgs to do list/no-check.svg" alt="noCheck">
-        </button>
-        </div>
+}
+ButtonCard.addEventListener("click",createCard);
 
-        <!--taks to make-->
-        <div class="Area" >
-            <p class="taskStyle"> testeofuwefuweuweutiewut9iausfuhfqwfhqidp</p>
-        </div>
-        <!--Checkz
-        ajeitar essas check boxes 
-        -->
-        <div class="Area">
-            <button class="buttonCheckStylesAndNo" >
-                <img src="imgs to do list/check.svg" alt="">
-            </button>
-        </div>
+// Card validaion
 
-        <div class="Area">
-            <p class="taskCheckStyle" rows="2">testeqwiejqij</p>
-        </div>
-        <!-- ajetar os botões de trash na tasks -->
-        <div class="trashButtonContainer">
-            <button class="trash">
-                <img src="imgs to do list/trash-01-svgrepo-com.svg" alt="">
-            </button>
-
-        </div>
-</div>
-</div>`;
-    gridCard.appendChild(Card);
-    Card+=Card
+function validaion(){
 }
 
+    // conseguimos pegar o valor do text area, agora é com você good luck
+
+    // precisamos fazer uma logica que impessa que um card sem nome possa ser criado
 
