4ae6106037e78e3974955f528b1ca9b1faa8092c
joaovictor <joaovictor0101@gmail.com>
2024-06-05 11:08:38 -0300

    implementando funcionalidade de tasks

--- ToDo.js
+++ ToDo.js
@@ -16,13 +16,13 @@ const ButtonCard = document.getElementById("plusHover");
 const inputDescription = document.getElementById("inputDescription") 
 const CreateTaskBar = document.getElementById("CreateTaskBar")
 const ContainerToDivClones = document.getElementById("cardContainerClones")
+const AreaTasks = document.getElementById("AreaTasks")
 let click = 1;
 
 // creation/validation
 
 function createCard(){ 
   click++
-    console.log(click)
   if ((click > 0) && (idNameList.value.trim() === "")){
     alert("add algo")
   }else{
@@ -34,6 +34,7 @@ function createCard(){
       idNameList.value =""
     inputDescription.value = ""
     CreateTaskBar.value = ""
+    AreaTasks = ""
   }
 }
 ButtonCard.addEventListener("click",createCard);
@@ -42,16 +43,44 @@ ButtonCard.addEventListener("click",createCard);
 
 
 const TaskButton = document.getElementById("TaskButton");
+const Tasks = document.getElementById("TaskNoCheck")
 
 // talvez depois mudar a forma de validação de card para permitir apenas tenha colocado algo nos campos
 
 // pick the value   
-// ajeitando questões de keybord
+
+// ajeitar questão de clonagem de tasks 
 function CreateTask(){
   console.log(CreateTaskBar.value)
+  if (!CreateTaskBar.value.trim() == ""){
+    let TaskNoCheck = document.createElement("div");
+    TaskNoCheck.innerHTML=`<div class="taskStylea">
+    <div class="trashButtonContainer">
+        <button class="trash">
+        <img src="imgs to do list/trash-01-svgrepo-com.svg" alt="">
+        </button>
+    </div>
+    <!--no check
+    mudar ajeitar essas chack boxes 
+    -->
+    <div class="NoAndCheck">
+        <div>
+            <img src="imgs to do list/no-check.svg" alt="">
+        </div>
+    </div>
+
+    <!--taks to make-->
+    <div class="Area1">
+    <p class="taskStyle">${CreateTaskBar.value}</p>
+    </div>
+</div>`
+    Tasks.appendChild(TaskNoCheck)
+    CreateTaskBar.value = ""
+  }else{
+    
+    alert("asdifef")
+  }
 
 }
 // ajeitando questões de keybord
-TaskButton.addEventListener("keyup",(e) => {
-  console.log(e.key)
-});
+TaskButton.addEventListener("click", CreateTask);
